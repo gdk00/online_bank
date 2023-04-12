00b1b76962aac119021f2e48d4f9e34c268bc732
apodkolzin <apodkolzin@totsystems.ru>
2023-04-12 19:56:06 +0300

    account

--- lite-bank/account/src/main/scala/misis/AccountApp.scala
+++ lite-bank/account/src/main/scala/misis/AccountApp.scala
@@ -3,20 +3,23 @@ package misis
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import com.typesafe.config.ConfigFactory
-import misis.db.InitDB
+import misis.kafka.Streams
+import misis.model.AccountUpdate
+import misis.repository.Repository
 import misis.route.Route
-import slick.jdbc.PostgresProfile.api._
 
 object AccountApp extends App {
     implicit val system: ActorSystem = ActorSystem("App")
     implicit val ec = system.dispatcher
-
-    implicit val db = Database.forConfig("database.postgres")
-
-    new InitDB()(ec, db).prepare()
-    private val route = new Route()
-
     val port = ConfigFactory.load().getInt("port")
+    val accountId = ConfigFactory.load().getInt("account.id")
+    val defAmount = ConfigFactory.load().getInt("account.amount")
 
+    private val repository = new Repository(accountId, defAmount)
+    private val streams = new Streams(repository)
+
+    streams.produceCommand(AccountUpdate(100))
+
+    private val route = new Route()
     Http().newServerAt("0.0.0.0", port).bind(route.routes)
 }
--- lite-bank/account/src/main/scala/misis/kafka/Streams.scala
+++ lite-bank/account/src/main/scala/misis/kafka/Streams.scala
@@ -1,43 +1,70 @@
 package misis.kafka
 
 import akka.actor.ActorSystem
-
 import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
 import akka.kafka.scaladsl.{Consumer, Producer}
 import akka.stream.scaladsl.{Flow, Sink, Source}
+import misis.repository.Repository
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer, StringSerializer}
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+import misis.model.{AccountUpdate, AccountUpdated}
 
 import scala.concurrent.ExecutionContext
 
-class Streams(implicit val system: ActorSystem, executionContext: ExecutionContext) {
+class Streams(repository: Repository)(implicit val system: ActorSystem, executionContext: ExecutionContext) {
 
     val consumerConfig = system.settings.config.getConfig("akka.kafka.consumer")
     val consumerSettings = ConsumerSettings(consumerConfig, new StringDeserializer, new StringDeserializer)
 
     val producerConfig = system.settings.config.getConfig("akka.kafka.producer")
     val producerSettings = ProducerSettings(producerConfig, new StringSerializer, new StringSerializer)
+                .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
-    //            .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
-    private val topic = "akka-kafka-topic"
-    val kafkaSource = Consumer.committableSource(consumerSettings, Subscriptions.topics(topic))
+    private val topicUpdate = "account-update"
+    private val topicUpdated = "account-updated"
+
+    Consumer.committableSource(consumerSettings, Subscriptions.topics(topicUpdate))
         .map(message => message.record.value())
-
-    def printFlow[T] = Flow[T].map { x =>
-        println(s"flow: ${x}")
-        x
-    }
-
-    val kafkaGraph = kafkaSource.via(printFlow).to(Sink.ignore)
-    kafkaGraph.run()
-
-
-    val produceGraph = Source(1 to 100)
-        .map(_.toString)
-        .map(value => new ProducerRecord[String, String](topic, value))
+        .map(body => decode[AccountUpdate](body))
+        .collect {
+            case Right(command) => command
+            case Left(error) => throw new RuntimeException(s"Ошибка при разборе сообщения $error")
+        }
+        .mapAsync(1)(command => repository.update(command.value))
+        .map(account => AccountUpdated(account.id, account.amount))
+        .map(event => event.asJson.noSpaces)
+        .map(value => new ProducerRecord[String, String](topicUpdated, value))
+        .log(s"Случилась ошибка при чтении топика $topicUpdate")
         .to(Producer.plainSink(producerSettings))
+        .run()
 
-    produceGraph.run()
+    Consumer.committableSource(consumerSettings, Subscriptions.topics(topicUpdated))
+        .map(message => message.record.value())
+        .map(body => decode[AccountUpdated](body))
+        .collect {
+            case Right(command) => command
+            case Left(error) => throw new RuntimeException(s"Ошибка при разборе сообщения $error")
+        }
+        .map { event =>
+            println(s"Получено событие: $event")
+            event
+        }
+        .log(s"Случилась ошибка при чтении топика $topicUpdated")
+        .to(Sink.ignore)
+        .run()
+
+
+    def produceCommand(command: AccountUpdate) = {
+        Source.single(command)
+            .map(command => command.asJson.noSpaces)
+            .map(value => new ProducerRecord[String, String](topicUpdate, value))
+            .to(Producer.plainSink(producerSettings))
+            .run()
+    }
 }
